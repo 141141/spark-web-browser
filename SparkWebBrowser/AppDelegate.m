25452b663b904aff1de0537c8b7e217e303c0c7a
Insleep <insleepdoesthings@gmail.com>
2016-05-08 09:31:18 -0500

    Prepare for tab support

--- SparkWebBrowser/AppDelegate.m
+++ SparkWebBrowser/AppDelegate.m
@@ -25,10 +25,10 @@
     if([urlToString isEqual: @"spark://about"]) {
         // This is not finished -- nothing happens when you visit spark://about (yet)
         NSLog(@"Secure Spark page loaded");
-        _securePageIndicator.hidden = NO;
-        _securePageIndicator.toolTip = @"You are viewing a secure Spark page.";
-        [_titleStatus setStringValue:@"About Spark"];
-        _stillLoading.hidden = YES;
+        self.securePageIndicator.hidden = NO;
+        self.securePageIndicator.toolTip = @"You are viewing a secure Spark page.";
+        [self.titleStatus setStringValue:@"About Spark"];
+        self.stillLoading.hidden = YES;
     }
 }
 
@@ -39,17 +39,19 @@
     NSString *appVersion = [infoDict objectForKey:@"CFBundleShortVersionString"]; // fetch the version number from info.plist
     NSString *buildNumber = [infoDict objectForKey:@"CFBundleVersion"]; // fetch the build number from info.plist
     window.titleVisibility = NSWindowTitleHidden; // for future purposes
-    [_webView setCustomUserAgent: @"Mozilla/5.0 (Macintosh; Intel Mac OS X) SparkWebBrowser/2.2.0.20806 (KHTML, like Gecko)"]; // static for now, will fix later
-    _ntNotSupported.hidden = YES;
-    _securePageIndicator.hidden = YES;
-    _stillLoading.hidden = NO;
-    _currentVersion.stringValue = [NSString stringWithFormat:@"%@.%@", appVersion, buildNumber];
+    [self.webView setCustomUserAgent: @"Mozilla/5.0 (Macintosh; Intel Mac OS X) SparkWebBrowser/2.2.0.20806 (KHTML, like Gecko)"]; // static for now, will fix later
+    self.ntNotSupported.hidden = YES;
+    self.securePageIndicator.hidden = YES;
+    self.stillLoading.hidden = NO;
+    self.currentVersion.stringValue = [NSString stringWithFormat:@"%@.%@", appVersion, buildNumber];
     self.window.backgroundColor = [NSColor colorWithRed:0.773 green:0.231 blue:0.212 alpha:1]; // title bar color in RGB
-    _aboutWindow.backgroundColor = [NSColor whiteColor];
+    self.aboutWindow.backgroundColor = [NSColor whiteColor];
+    
+    self.addressBar.action = @selector(takeStringURLFrom:);
 }
 
 - (IBAction)newTab:(id)sender {
-    
+    /*
     // No support for tabs in Spark -- display a label
     _ntNotSupported.hidden = NO;
     
@@ -58,7 +60,23 @@
     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
     dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
         _ntNotSupported.hidden = YES;
-    });
+    });*/
+    NSViewController *viewController = [[NSViewController alloc] initWithNibName: @"WebView" bundle: nil];
+    [self.viewControllers addObject: viewController];
+    
+    WebView *webView = viewController.view;
+    
+    NSTabView *tabView = self.tabView;
+    TabViewItem *item = [[TabViewItem alloc] initWithIdentifier: nil];
+    [item setLabel: @"title"];
+    [item setWebView: webView];
+    
+    NSView *view = item.view;
+    webView.frame = view.bounds;
+    webView.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
+    [view addSubview: webView];
+    
+    [tabView insertTabViewItem: item atIndex: [tabView numberOfTabViewItems]];
 }
 
 - (void)webView:(WebView *)sender didStartProvisionalLoadForFrame:(WebFrame *)frame
@@ -66,7 +84,7 @@
     // Only report feedback for the main frame.
     if (frame == [sender mainFrame]){
         NSString *url = [[[[frame provisionalDataSource] request] URL] absoluteString];
-        [_addressBar setStringValue:url];
+        [self.addressBar setStringValue:url];
     }
 }
 
@@ -74,8 +92,8 @@
 {
     // Only report feedback for the main frame.
     if (frame == [sender mainFrame]){
-        [_titleStatus setStringValue:title];
-        _stillLoading.hidden = YES;
+        [self.titleStatus setStringValue:title];
+        self.stillLoading.hidden = YES;
     }
 }
 
