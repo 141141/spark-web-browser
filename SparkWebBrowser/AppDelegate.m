826862de04ba510c35d065c21d21836cb510cb1f
Insleep <insleepdoesthings@gmail.com>
2016-07-14 13:01:02 -0500

    Create outlet + action

--- SparkWebBrowser/AppDelegate.m
+++ SparkWebBrowser/AppDelegate.m
@@ -105,6 +105,8 @@
         [[self.webView mainFrame] loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@", [defaults valueForKey:@"userHomepage"]]]]];
     }
 }
+- (IBAction)initSearch:(id)sender {
+}
 
 - (IBAction)setReleaseChannel:(id)sender {
     
