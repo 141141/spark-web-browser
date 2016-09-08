16e22129a07425354feb02ddcaf71af88d78de6d
Insleep <insleepdoesthings@gmail.com>
2016-09-08 16:26:10 -0500

    Add new settings interface

--- SparkWebBrowser/WelcomeScreen.m
+++ SparkWebBrowser/WelcomeScreen.m
@@ -1,12 +1,13 @@
 //
-//  Prefix header
-//
-//  The contents of this file are implicitly included at the beginning of every source file.
+//  WelcomeScreen.m
+//  Spark
 //
 //  Copyright (c) 2014-2016 Jonathan Wukitsch / InsleepTech
 //  This code is distributed under the terms and conditions of the GNU license.
 //  You may copy, distribute and modify the software as long as you track changes/dates in source files. Any modifications to or software including (via compiler) GPL-licensed code must also be made available under the GPL along with build & install instructions.
 
-#ifdef __OBJC__
-    #import <Cocoa/Cocoa.h>
-#endif
+#import "WelcomeScreen.h"
+
+@implementation WelcomeScreen
+
+@end
