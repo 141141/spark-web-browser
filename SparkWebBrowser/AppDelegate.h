05f762634b5a1d2849932be27c79f53818b947ee
Insleep <insleepdoesthings@gmail.com>
2016-09-23 23:19:17 -0500

    Update licensing info

--- SparkWebBrowser/AppDelegate.h
+++ SparkWebBrowser/AppDelegate.h
@@ -2,7 +2,7 @@
 //  AppDelegate.h
 //  Spark
 //
-//  Copyright (c) 2014-2016 Jonathan Wukitsch / InsleepTech
+//  Copyright (c) 2014-2016 Jonathan Wukitsch / Insleep
 //  This code is distributed under the terms and conditions of the GNU license.
 //  You may copy, distribute and modify the software as long as you track changes/dates in source files. Any modifications to or software including (via compiler) GPL-licensed code must also be made available under the GPL along with build & install instructions.
 
