29ce5da4cb21f51c2b77af1b521064919a2a2524
Insleep <insleepdoesthings@gmail.com>
2016-10-01 11:32:25 -0500

    Edit spacing

--- SparkWebBrowser/NSUserDefaults+ColorSupport.m
+++ SparkWebBrowser/NSUserDefaults+ColorSupport.m
@@ -10,14 +10,12 @@
 
 @implementation NSUserDefaults(myColorSupport)
 
-- (void)setColor:(NSColor *)aColor forKey:(NSString *)aKey
-{
+- (void)setColor:(NSColor *)aColor forKey:(NSString *)aKey {
     NSData *theData = [NSArchiver archivedDataWithRootObject:aColor];
     [self setObject:theData forKey:aKey];
 }
 
-- (NSColor *)colorForKey:(NSString *)aKey
-{
+- (NSColor *)colorForKey:(NSString *)aKey {
     NSColor *theColor = nil;
     NSData *theData = [self dataForKey:aKey];
     if (theData != nil)
